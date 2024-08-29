792a1dfdb418cb043a042dca8f968306321f7813
Ben Meadors <benmmeadors@gmail.com>
2024-08-29 16:20:20 -0500

    WIP

--- test/config.spec.ts
+++ test/config.spec.ts
@@ -3,26 +3,34 @@ import {
   describe,
   it,
 } from 'mocha';
+import { NodeSerialConnection } from '@meshtastic/js';
 
-describe('Config', () => {
-    describe('FunctionName', () => {
-        it('should do something', () => {
-            // Arrange
-            // Act
-            // Assert
-            expect(true).to.be.true;
-        });
+const connection = new NodeSerialConnection()
+connection.events.onMessagePacket.subscribe((packet) => {
+});
+connection.events.onLogEvent.subscribe((packet) => {
+});
+connection.events.onDeviceMetadataPacket.subscribe((packet) => {
+});
+connection.events.onDeviceDebugLog.subscribe((packet) => {
+});
+connection.events.onFromRadio.subscribe((packet) => {
+});
+connection.events.onDeviceStatus.subscribe((packet) => {
+});
+connection.events.onMyNodeInfo.subscribe((packet) => {
+});
+connection.events.onRoutingPacket.subscribe((packet) => {
+});
+connection.events.onConfigPacket.subscribe((packet) => {
+});
 
-        it('should handle edge case', () => {
-            // Arrange
-            // Act
-            // Assert
-            expect(true).to.be.true;
-        });
-    });
-    describe('AnotherFunctionName', () => {
-        it('should do something', () => {
-            // Arrange
+describe('Config', async () => {
+    await connection.connect({portPath: '/dev/tty.usbmodem1122101', baudRate: 115200, concurrentLogOutput: false})
+    await connection.configure();
+    describe('Device', () => {
+        it('should return valid config section', () => {
+            // ArrangeCounselor rubric
             // Act
             // Assert
             expect(true).to.be.true;
