dcac7e5673005f4d8a2b1f0f6e06877b689d7519
Ben Meadors <benmmeadors@gmail.com>
2024-09-24 15:23:31 -0500

    Python 3

--- src/setup.ts
+++ src/setup.ts
@@ -6,7 +6,7 @@ const devices = await DiscoverDevices();
 console.log(devices);
 
 devices.forEach(async device => {
-    exec(`python ./src/python/setup.py ${device.port} ${device.pio_env} ${device.arch}`, (error, stdout, stderr) => {
+    exec(`python3 ./src/python/setup.py ${device.port} ${device.pio_env} ${device.arch}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing the Python script: ${error}`);
             return;
